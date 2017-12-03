5616a2d10b14d9629b80e6d7d20c473e0c14a1ac
Alicja Gosiewska <alicjagosiewska@gmail.com>
2017-12-03 13:31:20 +0100

    update testGQ documentation

--- R/calculateResiduals.R
+++ R/calculateResiduals.R
@@ -1,8 +1,8 @@
-
 getResiduals <- function(model, y){
   y - predict(model)
 }
 
+#' @importFrom stats sd
 getStdResiduals <- function(model, y){
   residuals <- y - predict(model)
   return(residuals / sd(residuals))
