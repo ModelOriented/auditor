ed7eae4c18bc615e5303a25e4540ac74ff4422fc
Alicja Gosiewska <alicjagosiewska@gmail.com>
2018-02-26 12:24:50 +0100

    fix calculation gersiduals

--- R/calculateResiduals.R
+++ R/calculateResiduals.R
@@ -1,18 +1,18 @@
 getResiduals <- function(model, y, data){
-  if (is.numeric(y)) {
-    res <-  y - predict(model, type = "response", newdata = data)
-  } else {
+  if ("randomForest" %in% class(model)) {
     res <- 1 - predict(model, type = "prob", newdata = data)[cbind(1:nrow(data),y)]
+  } else {
+    res <-  y - predict(model, type = "response", newdata = data)
   }
   return(res)
 }
 
 #' @importFrom stats sd
 getStdResiduals <- function(model, y, data){
-  if(is.numeric(y)) {
-    res <- y - getResiduals(model, y, data)
-  } else {
+  if("randomForest" %in% class(model)) {
     res <- 1 - predict(model, type = "prob", newdata = data)[cbind(1:nrow(data),y)]
+  } else {
+    res <- y - getResiduals(model, y, data)
   }
   return(res / sd(res))
 }
