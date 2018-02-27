1757c6e24d631ad40d7718ee3bbd4e8d7826bcc8
Alicja Gosiewska <alicjagosiewska@gmail.com>
2018-02-27 21:34:26 +0100

    fix factors for regression
    #20

--- R/calculateResiduals.R
+++ R/calculateResiduals.R
@@ -3,6 +3,7 @@ getResiduals <- function(model, y, data, predict.function){
   if (!is.null(model$type) && model$type == "classification") {
     res <- 1 - predict.function(model, data)[cbind(1:nrow(data),y)]
   } else {
+    if(is.factor(y)) y <- as.numeric(y) - 1
     res <-  y - predict(model, type = "response", newdata = data)
   }
 
