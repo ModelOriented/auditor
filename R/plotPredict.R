1d0c644d808e7bb106c8f6b392c3e12f589d84c2
Alicja Gosiewska <alicjagosiewska@gmail.com>
2018-05-04 13:35:21 +0200

    Add plotPrediction

---
+++ R/plotPredict.R
@@ -0,0 +1,41 @@
+#' @title Observed response values vs Predicted response
+#'
+#' @description Plot of predicted vs actual response.
+#'
+#'
+#' @param object An object of class modelAudit
+#' @param ... other modelAudit objects to be plotted together
+#'
+#' @seealso \code{\link{plot.modelAudit}}
+#'
+#' @import ggplot2
+#'
+#' @export
+plotPrediction <- function(object, ..., variable=NULL){
+  residuals <- values <- NULL
+
+  df <- generatePredictionDF(object, variable)
+
+  dfl <- list(...)
+  if (length(dfl) > 0) {
+    for (resp in dfl) {
+      if(class(resp)=="modelAudit"){
+        df <- rbind( df, generatePredictionDF(resp, variable) )
+      }
+    }
+  }
+
+  ggplot(df, aes(observed, predicted, color = label)) +
+    geom_point() +
+    geom_abline(slope = 1, intercept = 0) +
+    xlab("Observed response") +
+    ylab("Predicted values") +
+    ggtitle(paste0("Observed vs Predicted", variable)) +
+    theme_light()
+}
+
+
+generatePredictionDF <- function(object, variable){
+  resultDF <- data.frame(predicted = object$fitted.values, observed = object$y, label = object$label)
+  return(resultDF)
+}
