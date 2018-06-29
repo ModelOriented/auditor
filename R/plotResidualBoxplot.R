7c7e2e33381b85d729890d7590a9f5b2e9e59b93
Alicja Gosiewska <alicjagosiewska@gmail.com>
2018-06-29 12:25:03 +0200

    add plotResidualBoxplot()

---
+++ R/plotResidualBoxplot.R
@@ -0,0 +1,56 @@
+#' @title Plot Boxplots of Residuals
+#'
+#' @description A plot of residuals.
+#'
+#' @param object An object of class modelAudit.
+#' @param ... Other modelAudit objects to be plotted together.
+#'
+#' @examples
+#' library(car)
+#' lm_model <- lm(prestige~education + women + income, data = Prestige)
+#' lm_au <- audit(lm_model, data = Prestige, y = Prestige$prestige)
+#' plotResidual(lm_au)
+#'
+#' library(randomForest)
+#' rf_model <- randomForest(prestige~education + women + income, data = Prestige)
+#' rf_au <- audit(rf_model, data = Prestige, y = Prestige$prestige)
+#' plotResidualBoxplot(lm_au, rf_au)
+#'
+#' @seealso \code{\link{plot.modelAudit}}
+#'
+#' @import ggplot2
+#'
+#' @export
+plotResidualBoxplot <- function(object, ...){
+  residuals <- RMSE <- label <- NULL
+
+  df <- generateResidualBoxplotDF(object)
+
+  dfl <- list(...)
+  if (length(dfl) > 0) {
+    for (resp in dfl) {
+      if(class(resp)=="modelAudit"){
+        df <- rbind(df, generateResidualBoxplotDF(resp) )
+      }
+    }
+  }
+
+
+  ggplot(df, aes(label, abs(residuals), fill = label)) +
+    geom_boxplot(outlier.shape = NA) +
+    stat_summary(fun.y=function(x){sqrt(mean(x^2))}, geom="point", shape=20, size=10, color="red", fill="red") +
+    xlab("") +
+    ylab("") +
+    ggtitle("Boxplots of | residuals |", "Red dot stands for root mean square of residuals") +
+    theme_light() +
+    coord_flip()
+}
+
+
+generateResidualBoxplotDF <- function(object){
+  resultDF <- data.frame(residuals =object$residuals)
+  resultDF$label <- object$label
+  resultDF$RMSE <- scoreRMSE(object)$score
+  return(resultDF)
+}
+
