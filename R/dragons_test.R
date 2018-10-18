df5b9fc68f6ffbdcebb24f89eebb0cbcae273cb3
Agnieszka Ciepielewska <agnieszkac27@gmail.com>
2018-10-18 10:17:01 +0200

    adding dragon datasets

---
+++ R/dragons_test.R
@@ -0,0 +1,11 @@
+#' @title dragons_test
+#' @description dragons_test is an artificial data set.
+#' It consists of 1000 observations.
+#'
+#' @name dragons_test
+#' @docType data
+#'
+#' @examples
+#' data("dragons_test", package = "auditor")
+#' head(dragons_test)
+NULL
