d35a97abf7ed1b1ac238e0e8c82957299045d4d0
Agnieszka Ciepielewska <agnieszkac27@gmail.com>
2018-10-24 16:38:13 +0200

    document data set

--- R/dragons.R
+++ R/dragons.R
@@ -1,11 +1,18 @@
 #' @title dragons
-#' @description dragons is an artificial data set.
-#' It consists of 2000 observations.
+#' Datasets \code{dragons} and \code{dragons_test} are artificial,
+#' generated form the same model.
 #'
-#' @name dragons
+#' \itemize{
+#' \item life.len - dragon's life length
+#' \item teeth.len - dragon's teeth length 
+#' \item colour - coulour of the dragon
+#' \item height - dragon's height
+#' \item herd.size - the size of the dragon herd
+#' }
+#'
 #' @docType data
-#'
-#' @examples
-#' data("dragons", package = "auditor")
-#' head(dragons)
+#' @keywords dragons
+#' @name dragons
+#' @usage data(dragons)
+#' @format a data frame with 2000 rows and 5 columns
 NULL
