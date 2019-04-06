4f58231a91fcd87b985da6a79499daf4e3dd6429
hbaniecki <h.baniecki@student.mini.pw.edu.pl>
2019-04-06 11:24:56 +0200

    fix stuff

--- inst/d3js/plotResidualsMany.js
+++ inst/d3js/plotResidualsMany.js
@@ -35,8 +35,8 @@ residual(data);
 
 svg.selectAll("text")
   .style('font-family', 'Fira Sans, sans-serif');
-    
-// plot function    
+
+// plot function
 function residual(data){
   var pointData = data[0], smoothData = data[1];
   for (var i=0; i<n; i++){
@@ -46,21 +46,21 @@ function residual(data){
 }
 
 function singlePlot(modelName, pointData, smoothData, i) {
-    
+
     var x = d3.scaleLinear()
           .range([plotLeft + 10, plotLeft + plotWidth - 10])
           .domain([minVariable, maxVariable]);
-    
+
     var y = d3.scaleLinear()
-          .range([plotTop + plotHeightx, plotTop])
+          .range([plotTop + plotHeight, plotTop])
           .domain([minResidual, maxResidual]);
-    
-    // function to draw smooth lines  
+
+    // function to draw smooth lines
     var line = d3.line()
           .x(function(d) { return x(d.val); })
-          .y(function(d) { return y(d.smooth); }) 
+          .y(function(d) { return y(d.smooth); })
           .curve(d3.curveMonotoneX);
-    
+
     // add plot things only once
     if (i==1) {
       svg.append("text")
@@ -68,7 +68,7 @@ function singlePlot(modelName, pointData, smoothData, i) {
           .attr("x", plotLeft)
           .attr("y", plotTop - 60)
           .text(chartTitle);
-      
+
       svg.append("text")
           .attr("class", "axisTitle")
           .attr("transform", "rotate(-90)")
@@ -77,23 +77,23 @@ function singlePlot(modelName, pointData, smoothData, i) {
           .attr("text-anchor", "middle")
           .text(yTitle);
     }
-    
+
     svg.append("text")
         .attr("class","smallTitle")
         .attr("x", plotLeft)
         .attr("y", plotTop - 15)
         .text(modelName);
-      
+
     // find 5 nice ticks with max and min - do better than d3
     var domain = x.domain();
     var tickValues = d3.ticks(domain[0], domain[1],5);
-      
+
     switch (tickValues.length){
       case 3:
         tickValues.unshift(domain[0]);
         tickValues.push(domain[1]);
         break;
-          
+
       case 4:
         if(Math.abs(domain[0] - tickValues[0]) < Math.abs(domain[1] - tickValues[3])){
           tickValues.shift();
@@ -105,14 +105,14 @@ function singlePlot(modelName, pointData, smoothData, i) {
           tickValues.unshift(domain[0]);
         }
         break;
-          
+
       case 5:
         tickValues.pop();
         tickValues.shift();
         tickValues.push(domain[1]);
         tickValues.unshift(domain[0]);
         break;
-          
+
       case 6:
         if(Math.abs(domain[0] - tickValues[0]) < Math.abs(domain[1] - tickValues[3])){
           tickValues.pop();
@@ -128,7 +128,7 @@ function singlePlot(modelName, pointData, smoothData, i) {
           tickValues.unshift(domain[0]);
         }
         break;
-          
+
       case 7:
         tickValues.pop();
         tickValues.pop();
@@ -137,74 +137,77 @@ function singlePlot(modelName, pointData, smoothData, i) {
         tickValues.push(domain[1]);
         tickValues.unshift(domain[0]);
     }
-      
+
     // axis and grid
     var xAxis = d3.axisBottom(x)
               .tickValues(tickValues)
               .tickSizeInner(0)
               .tickPadding(15);
-                        
+
     xAxis = svg.append("g")
               .attr("class", "axisLabel")
               .attr("transform", "translate(0,"+ (plotTop + plotHeight) + ")")
               .call(xAxis);
-      
-    var yGrid = svg.append("g")			
+
+    var yGrid = svg.append("g")
              .attr("class", "grid")
              .attr("transform", "translate(" + plotLeft + ",0)")
              .call(d3.axisLeft(y)
-                    .ticks(10) 
+                    .ticks(10)
                     .tickSize(-plotWidth)
                     .tickFormat("")
             ).call(g => g.select(".domain").remove());
-    
-    if (i%2 === 1) {            
+
+    if (i%2 === 1) {
       var yAxis = d3.axisLeft(y)
               .ticks(5)
               .tickSize(0);
-        
+
       yAxis = svg.append("g")
               .attr("class", "axisLabel")
               .attr("transform","translate(" + plotLeft + ",0)")
               .call(yAxis)
               .call(g => g.select(".domain").remove());
     }
-    
+
     if (background === true){
       // draw grey scatter and smooth
       for (var j=0; j<n; j++){
         if (modelNames[j] !== modelName){
           let tempName = modelNames[j];
-          
+
           if (points === true) {
-            
+
             let scatter = svg.selectAll()
                 .data(pointData[tempName])
                 .enter();
-                
+
             scatter.append("circle")
                 .attr("class", "dot " + tempName)
                 .attr("id", tempName)
                 .attr("cx", d => x(d.val))
                 .attr("cy", d => y(d.res))
                 .attr("r", 1)
-                .style("fill", greyColor); 
+                .style("fill", greyColor)
+                .style("opacity", 0.5);
           }
-          
+
           if (smooth === true) {
-            
+
             svg.append("path")
                 .data([smoothData[tempName]])
                 .attr("class", "smooth " + tempName)
                 .attr("id", tempName)
                 .attr("d", line)
                 .style("fill", "none")
-                .style("stroke", greyColor); 
+                .style("stroke", greyColor)
+                .style("stroke-width", 2)
+                .style("opacity", 0.5);
           }
         }
-      } 
+      }
     }
-    
+
     // scatter
     if (points === true) {
       svg
@@ -217,21 +220,21 @@ function singlePlot(modelName, pointData, smoothData, i) {
         .attr("cx", d => x(d.val))
         .attr("cy", d => y(d.res))
         .attr("r", 1)
-        .style("fill", pointColor)
-        .style("opacity", 0.5);
+        .style("fill", pointColor);
     }
-      
-    // smooth line  
+
+    // smooth line
     if (smooth === true) {
       svg.append("path")
-        .data([smoothData[modelName]]) 
+        .data([smoothData[modelName]])
         .attr("class", "smooth " + modelName)
         .attr("id", modelName)
         .attr("d", line)
         .style("fill", "none")
-        .style("stroke", smoothColor);  
+        .style("stroke", smoothColor)
+        .style("stroke-width", 2);
     }
-    
+
     if (i%2 === 1){
       plotLeft += (25 + plotWidth);
     }
--- inst/d3js/plotResidualsSingle.js
+++ inst/d3js/plotResidualsSingle.js
@@ -26,18 +26,18 @@ var modelNames = Object.keys(data[k]);
 var colors = getColors(3, "point");
 var pointColor = colors[k], dPointColor = colors[k];
 var smoothColor = colors[0], dSmoothColor = colors[0];
-var greyColor = colors[2];
+var greyColor = colors[2], dOpacity = 1;
 
 residual(data);
 
 if (n!=1) {
-    svg.select("g.legend").select("circle.legendDot").dispatch("click");   
+    svg.select("g.legend").select("circle.legendDot").dispatch("click");
 }
 
 svg.selectAll("text")
   .style('font-family', 'Fira Sans, sans-serif');
-    
-// plot function    
+
+// plot function
 function residual(data){
   var pointData = data[0], smoothData = data[1];
   for (var i=0; i<n; i++){
@@ -47,21 +47,21 @@ function residual(data){
 }
 
 function singlePlot(modelName, pData, sData, i) {
-    
+
     var x = d3.scaleLinear()
           .range([margin.left + 10, margin.left + plotWidth - 10])
           .domain([minVariable, maxVariable]);
-    
+
     var y = d3.scaleLinear()
           .range([margin.top + plotHeight, margin.top])
           .domain([minResidual, maxResidual]);
-    
-    // function to draw smooth lines  
+
+    // function to draw smooth lines
     var line = d3.line()
           .x(function(d) { return x(d.val); })
-          .y(function(d) { return y(d.smooth); }) 
+          .y(function(d) { return y(d.smooth); })
           .curve(d3.curveMonotoneX);
-    
+
     // add plot things only once
     if (i==1) {
       svg.append("text")
@@ -69,7 +69,7 @@ function singlePlot(modelName, pData, sData, i) {
           .attr("x", margin.left)
           .attr("y", margin.top - 60)
           .text(chartTitle);
-          
+
       svg.append("text")
           .attr("class", "axisTitle")
           .attr("transform", "rotate(-90)")
@@ -77,24 +77,24 @@ function singlePlot(modelName, pData, sData, i) {
           .attr("x", -(margin.top + plotHeight/2))
           .attr("text-anchor", "middle")
           .text(yTitle);
-          
-      svg.append("text")    
+
+      svg.append("text")
           .attr("class", "axisTitle")
           .attr("transform",
                 "translate(" + (margin.left + plotWidth/2) + " ," + (margin.top + plotHeight + 50) + ")")
           .attr("text-anchor", "middle")
           .text(variableName);
-      
+
       // find 5 nice ticks with max and min - do better than d3
       var domain = x.domain();
       var tickValues = d3.ticks(domain[0], domain[1],5);
-      
+
       switch (tickValues.length){
         case 3:
           tickValues.unshift(domain[0]);
           tickValues.push(domain[1]);
           break;
-          
+
         case 4:
           if(Math.abs(domain[0] - tickValues[0]) < Math.abs(domain[1] - tickValues[3])){
             tickValues.shift();
@@ -106,14 +106,14 @@ function singlePlot(modelName, pData, sData, i) {
             tickValues.unshift(domain[0]);
           }
           break;
-          
+
         case 5:
           tickValues.pop();
           tickValues.shift();
           tickValues.push(domain[1]);
           tickValues.unshift(domain[0]);
           break;
-          
+
         case 6:
           if(Math.abs(domain[0] - tickValues[0]) < Math.abs(domain[1] - tickValues[3])){
             tickValues.pop();
@@ -129,7 +129,7 @@ function singlePlot(modelName, pData, sData, i) {
             tickValues.unshift(domain[0]);
           }
           break;
-          
+
         case 7:
           tickValues.pop();
           tickValues.pop();
@@ -138,48 +138,48 @@ function singlePlot(modelName, pData, sData, i) {
           tickValues.push(domain[1]);
           tickValues.unshift(domain[0]);
       }
-      
+
       // axis and grid
       var xAxis = d3.axisBottom(x)
                 .tickValues(tickValues)
                 .tickSizeInner(0)
                 .tickPadding(15);
-                        
+
       xAxis = svg.append("g")
                 .attr("class", "axisLabel")
                 .attr("transform", "translate(0,"+ (margin.top + plotHeight) + ")")
                 .call(xAxis);
-      
-      var yGrid = svg.append("g")			
+
+      var yGrid = svg.append("g")
                .attr("class", "grid")
                .attr("transform", "translate(" + margin.left + ",0)")
                .call(d3.axisLeft(y)
-                      .ticks(10) 
+                      .ticks(10)
                       .tickSize(-plotWidth)
                       .tickFormat("")
               ).call(g => g.select(".domain").remove());
-                
+
       var yAxis = d3.axisLeft(y)
               .ticks(5)
               .tickSize(0);
-      
+
       yAxis = svg.append("g")
               .attr("class", "axisLabel")
               .attr("transform","translate(" + labelsMargin + ",0)")
               .call(yAxis)
               .call(g => g.select(".domain").remove());
-              
+
       if (n!=1) {
-        
+
         var legend = svg.selectAll(".legend")
               .data(modelNames)
               .enter()
               .append("g")
               .attr("class", "legend");
-              
+
         // add legend
         var textWidth = [];
-        
+
         legend.append("text")
                 .attr("dy", ".6em")
                 .attr("class", "legendLabel")
@@ -189,21 +189,21 @@ function singlePlot(modelName, pData, sData, i) {
                     var thisWidth = this.getComputedTextLength();
                     textWidth.push(thisWidth);
                 });
-        
+
         // make nice legend text and boxes
         var maxLength = 2*d3.max(textWidth)+10;
-        legend.attr("transform", function(d, i) { 
-            return "translate(" + (margin.left+plotWidth - maxLength*(n-i)) + 
-            "," + (margin.top - 20) + ")"; 
+        legend.attr("transform", function(d, i) {
+            return "translate(" + (margin.left+plotWidth - maxLength*(n-i)) +
+            "," + (margin.top - 20) + ")";
         });
-        
+
         var activeLink = modelName.replace(/\s/g, '');
-        
+
         legend.append("rect")
                 .attr("width", 8)
                 .attr("height", 8)
                 .attr("class", "legendBox");
-        
+
         // legend functions
         legend.append("circle")
                 .attr("class", "legendDot")
@@ -218,7 +218,7 @@ function singlePlot(modelName, pData, sData, i) {
                 .attr("id", function (d, i) {
                           return d.replace(/\s/g, '');
                         })
-                .on("mouseover",function(){   
+                .on("mouseover",function(){
                       // change cursor
                       if (activeLink === this.id){
                         d3.select(this).style("cursor", "auto");
@@ -227,55 +227,56 @@ function singlePlot(modelName, pData, sData, i) {
                       }
                 })
                 .on("click", function(){
-                      
-                      svg.selectAll("circle.legendDot")          
+
+                      svg.selectAll("circle.legendDot")
                             .style("fill", greyColor)
                             .style("opacity", 0.5);
-                          
+
                       svg.selectAll(".point" + activeLink)
                             .style("fill", greyColor)
                             .style("opacity", 0.5);
                       svg.selectAll(".smooth" + activeLink)
                             .style("stroke", greyColor)
                             .style("opacity", 0.5);
-                      
+
                       activeLink = this.id;
-                      
+
                       d3.select(this)
                           .style("fill", pointColor)
                           .style("opacity", 1);
-                        
+
                       svg.selectAll(".point" + activeLink)
                             .style("fill", pointColor)
                             .style("opacity", 1);
                       svg.selectAll(".smooth" + activeLink)
                             .style("stroke", smoothColor)
                             .style("opacity", 1);
-                      
+
                       // effort to bring plot to the top
                       svg.selectAll("#" + activeLink).each(function() {
                            this.parentNode.appendChild(this);
-                      }); 
+                      });
                   });
-                
+
           dPointColor = greyColor;
           dSmoothColor = greyColor;
-          
+          dOpacity = 0.5;
+
         } else { // end of n!=1
           svg.append("text")
             .attr("x", margin.left)
             .attr("y", margin.top - 15)
             .attr("class","smallTitle")
             .text(modelName);
-            
+
         }
     } //end of i==1
-    
+
     let tModelName = modelName.replace(/\s/g, '');
-    
+
     // scatter
     if (points === true) {
-      
+
       svg.selectAll()
         .data(pData)
         .enter()
@@ -286,19 +287,19 @@ function singlePlot(modelName, pData, sData, i) {
         .attr("cy", d => y(d.res))
         .attr("r", 1)
         .style("fill", dPointColor)
-        .style("opacity", 0.5);
+        .style("opacity", dOpacity);
     }
-      
-    // smooth line 
+
+    // smooth line
     if (smooth === true) {
       svg.append("path")
-        .data([sData]) 
+        .data([sData])
         .attr("class", "smooth" + tModelName)
         .attr("id", tModelName)
         .attr("d", line)
         .style("fill", "none")
         .style("stroke", dSmoothColor)
-        .style("opacity", 0.5)
-        .style("stroke-width", 2);  
+        .style("opacity", dOpacity)
+        .style("stroke-width", 2);
     }
 }
