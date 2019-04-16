660e74f96e47bb059bb314a556451ea9267ba5a3
Tomasz Mikołajczyk <tmikolajczyk@users.noreply.github.com>
2019-04-16 23:09:57 +0200

    Plot residuals (#45)
    * adding new skin (theme) for plots
    
    * plotResiduals with a new skin applied
    
    * removing theme file from branch, it will go to the master from now on
    
    * making codes for plotResidual more clean and straight

--- R/plotResidual.R
+++ R/plotResidual.R
@@ -27,49 +27,74 @@
 #' @importFrom ggrepel geom_text_repel
 #'
 #' @export
-plotResidual <- function(object, ..., variable=NULL, points = TRUE, lines = FALSE,
-                         std.residuals = FALSE, nlabel = 0){
+plotResidual <- function(object, ..., variable = NULL, points = TRUE, lines = FALSE,
+                         std.residuals = FALSE, nlabel = 0) {
+
   if(!("modelResiduals" %in% class(object) || "modelAudit" %in% class(object))) stop("The function requires an object created with audit() or modelResiduals().")
   if("modelResiduals" %in% class(object)) variable <- object$variable[1]
   if(!("modelResiduals" %in% class(object))) object <- modelResiduals(object, variable)
 
-
   res <- std.res <- val <- label <- index <- NULL
 
-  ylabel <- ifelse(std.residuals == TRUE, "standardized residuals", "residuals")
   df <- object
-
   dfl <- list(...)
   if (length(dfl) > 0) {
     for (resp in dfl) {
-      if("modelAudit" %in% class(resp)) df <- rbind( df, modelResiduals(resp, variable) )
-      if("modelResiduals" %in% class(resp)) df <- rbind(df, resp)
+      if ("modelAudit" %in% class(resp)) df <- rbind(df, modelResiduals(resp, variable))
+      if ("modelResiduals" %in% class(resp)) df <- rbind(df, resp)
     }
   }
 
-  maybe_points <- if (points == TRUE) df else df[0, ]
-  maybe_lines <- if (lines == TRUE) df else df[0, ]
-  maybe_labels <- df[order(abs(df$res), decreasing = TRUE),][0:nlabel, ]
-
-  if(is.na(df$variable[1])) variable <- NULL
-
-  if (is.null(variable)) {
-    title <- "Residuals"
-  } else {
-    title <- paste0("Residuals vs ", variable)
+  ytitle <- "Residuals"
+  if (std.residuals == TRUE) {
+    res <- std.res
+    ytitle <- "Standardized residuals"
   }
 
-  if(std.residuals == TRUE) {p <- ggplot(df, aes(val, std.res, color = label))}
-  else {p <- ggplot(df, aes(val, res, color = label))}
+  # data frames for each geom
+  maybe_points <- if (points == TRUE) df else df[0, ]
+  maybe_lines  <- if (lines  == TRUE) df else df[0, ]
+  maybe_labels <- df[order(abs(df$res), decreasing = TRUE),][0:nlabel, ] # ?????
 
-  p + geom_point(data = maybe_points, alpha = 1, stroke=0) +
-      geom_smooth(data = maybe_lines, method = "loess", se = FALSE, size = 2) +
-      geom_text_repel(data = maybe_labels, aes(label = as.character(index)),
-                hjust=-0.2,vjust=-0.2, show.legend = FALSE) +
-      xlab(variable) +
-      ylab(ylabel) +
-      ggtitle(title) +
-      theme_light()
+  # depending of how many models are presented (1 or more) - colors and other values are changing
+  colours <- theme_drwhy_colors(length(unique(df$label)))
+
+  p <- ggplot() +
+    geom_point(data = df, aes(val, res, color = label), alpha = ifelse(lines == TRUE, 0.65, 1), stroke = 0) +
+    geom_line(data = maybe_lines,
+              aes(val, res, colour = factor(label, levels = rev(levels(maybe_lines$label)))),
+              stat = "smooth",
+              method = "loess",
+              se = FALSE,
+              size = 1,
+              show.legend = TRUE) +
+    scale_color_manual(values = colours) +
+    xlab("residual i") +
+    ylab("residual i+1") +
+    ggtitle("Autocorrelation plot") +
+    theme_drwhy() +
+    theme(axis.line.x = element_line(color = "#371ea3"))+
+    geom_text_repel(data = maybe_labels,
+                    aes(val, res, label = as.character(index)),
+                    hjust = -0.2,
+                    vjust = -0.2,
+                    color = theme_drwhy_colors(1),
+                    size = 3.3,
+                    show.legend = FALSE)
+
+  chart_title <- "Residuals"
+
+  if (is.na(df$variable[1])) {
+    variable <- "observations"
+    p <- p + scale_x_continuous(breaks = 5, labels = "")
+  } else {
+    chart_title <- paste0("Residuals vs ", variable)
+    p <- p + scale_x_continuous(breaks = scales::pretty_breaks())
+  }
+
+  p <- p + xlab(variable) + ylab(ytitle) + ggtitle(chart_title)
+
+  return(p)
+
 }
-
 
