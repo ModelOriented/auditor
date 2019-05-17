7a29d218afc5c5ff88c1fba1977a0e64fe1e664a
Tomasz Mikolajczyk <t.mikolajczyk@gmail.com>
2019-05-17 10:03:09 +0200

    plotPrediction with external functions

--- R/plotPrediction.R
+++ R/plotPrediction.R
@@ -6,10 +6,12 @@
 #' @param object An object of class modelAudit or modelResiduals.
 #' @param ... Other modelAudit or modelResiduals objects to be plotted together.
 #' @param variable Only for modelAudit objects. Name of model variable to order residuals.
-#' If value is NULL data order is taken. If value is "Observed response" the data is ordered
-#' by a vector of actual response (\code{y} parameter passed to the \code{\link{audit}} function).
+#' If value is NULL the data is ordered by a vector of actual response (\code{y} parameter
+#' passed to the \code{\link{audit}} function). One can also pass any name of any other variable
+#' in the data set. If \code{variable = ""} is set, unordered observations are presented.
 #' @param smooth Logical, indicates whenever smooth line should be added.
-#' @param abline Logical, indicates whenever function y=x shoul be added.
+#' @param abline Logical, indicates whenever function y = x shoul be added. Works only
+#' with \code{variable = NULL} which is a default option.
 #'
 #' @examples
 #' library(car)
@@ -29,71 +31,54 @@
 #' @export
 plotPrediction <- function(object, ..., variable = NULL, smooth = FALSE, abline = FALSE) {
 
-  if(!("modelResiduals" %in% class(object) || "modelAudit" %in% class(object))) {
-    stop("The function requires an object created with audit() or modelResiduals().")
-  }
-  if("modelResiduals" %in% class(object)) variable <- object$variable[1]
-  if(!("modelResiduals" %in% class(object))) object <- modelResiduals(object, variable)
-
+  # some safeguard
   val <- fitted.values <- label <- NULL
 
-  # data frame for the chart
-  df <- object
-  dfl <- list(...)
-  if (length(dfl) > 0) {
-    for (resp in dfl) {
-      if("modelAudit" %in% class(resp)) df <- rbind(df, modelResiduals(resp, variable) )
-      if("modelResiduals" %in% class(resp)) df <- rbind(df, resp)
-    }
-  }
+  # check if passed object is of class "modelResiduals" or "modelAudit"
+  check_object(object, type = "res")
 
-  # data for additional geoms
+  # data frame for ggplot object
+  df <- make_dataframe(object, ..., variable = variable, type = "res")
+
+  # data frame for extra geoms
   maybe_smooth <- if (smooth == TRUE) df else df[0, ]
 
+  # set value for label of the X axis
+  x_lab <- as.character(df$variable[1])
+
   # colors for model(s)
-  colours <- theme_drwhy_colors(length(unique(df$label)))
+  colours <- rev(theme_drwhy_colors(length(levels(df$label))))
 
   # main chart
-  p <- ggplot(data = df, aes(val, fitted.values)) +
-    geom_point(aes(color = label),
-               alpha = ifelse(smooth == TRUE, 0.65, 1),
-               stroke = 0) +
-    scale_color_manual(values = colours) +
-    theme_drwhy() +
-    theme(axis.line.x = element_line(color = "#371ea3"))
+  p <- ggplot(data = df, aes(val, fitted.values))
+
+  # scatter plot for the main model
+  p <- p + drwhy_geom_point(df, smooth, alpha_val = 0.65)
+
+  # smoot curve for the main model
+  if (smooth == TRUE)
+    p <- p + drwhy_geom_smooth(maybe_smooth)
 
   # abline, drawn only when response variable was passed
-  if (abline == TRUE & !is.na(df$variable[1])) {
+  if (abline == TRUE & x_lab == "Target variable")
     p <- p + geom_abline(colour = "#ae2c87", alpha = 0.65)
-  }
 
-  # smoot curve(s)
-  if (smooth == TRUE) {
-    p <- p + geom_smooth(data = maybe_smooth,
-                         aes(colour = factor(label, levels = rev(levels(maybe_smooth$label)))),
-                         stat = "smooth",
-                         method = "gam",
-                         formula = y ~ s(x, bs = "cs"),
-                         se = FALSE,
-                         size = 1,
-                         show.legend = TRUE)
-  }
+  # theme, colours, titles, axes, scales, etc.
+  p <- p + theme_drwhy() +
+    theme(axis.line.x = element_line(color = "#371ea3")) +
+    scale_color_manual(values = rev(colours),
+                       breaks = levels(df$label))
 
-  # axes, scales, titles, etc.
   chart_title <- "Predicted"
 
-  if (is.na(df$variable[1])) {
-    variable <- "Observations"
-    p <- p + scale_x_continuous(breaks = 5, labels = "")
-  } else {
-    chart_title <- paste0(chart_title, " vs ", variable)
+  if (x_lab != "Observations") {
     p <- p + scale_x_continuous(breaks = scales::pretty_breaks())
+    if (x_lab != "Target variable") chart_title <- paste0(chart_title, " vs ", x_lab)
+  } else {
+    p <- p + scale_x_continuous(breaks = 5, labels = "")
   }
 
-  p <- p +
-    xlab(variable) +
-    ylab("Predicted values") +
-    ggtitle(chart_title)
+  p <- p + xlab(x_lab) + ylab("Predicted values") + ggtitle(chart_title)
 
   return(p)
 }
---
+++ R/plot_support_function.R
@@ -0,0 +1,78 @@
+
+#' @title Check object
+#'
+#' @description Checks if the object is of desired class
+#'
+#' @param object Object passed to the function
+check_object <- function(object, type = "res") {
+
+  model_type <- switch(type,
+                       "res" = "modelResiduals",
+                       "eva" = "modelEvaluation")
+
+  if (!(model_type %in% class(object) || "modelAudit" %in% class(object))) {
+    stop("The function requires an object created with audit() or modelResiduals().")
+  }
+}
+
+
+#' @title Make data frame
+#'
+#' @description Makes data frame(s) from passed models
+#'
+#' @param object Objecta passed to the function
+make_dataframe <- function(object, ..., variable, type = "res") {
+
+  if (type == "res" & !"modelResiduals"  %in% class(object)) object <- modelResiduals(object, variable)
+  if (type == "eva" & !"modelEvaluation" %in% class(object)) object <- attributes(modelEvaluation(object))$CGains
+
+  dfl <- list(...)
+  if (length(dfl) > 0) {
+    for (resp in dfl) {
+      if (type == "res") {
+        if ("modelAudit" %in% class(resp)) resp <- modelResiduals(resp, variable)
+        if ("modelResiduals" %in% class(resp)) object <- rbind(object, resp)
+      }
+      if (type == "eva") {
+        if ("modelAudit" %in% class(temp)) resp <- modelEvaluation(resp)
+        if ("modelEvaluation" %in% class(resp)) object <- rbind(object, attributes(resp)$CGains)
+      }
+    }
+  }
+  return(object)
+}
+
+
+#' @title DrWhy's wrapper for geom_point function
+#'
+#' @description Function which draws point layers in desired order
+#'
+#' @param df Data frame prepared by (\code{make_dataframe}) function
+#' @param smooth Logical
+drwhy_geom_point <- function(df, smooth = FALSE, alpha_val) {
+  # ordering data to get rigth order of points on the plot
+  df <- df[order(-as.numeric(factor(df$label))), ]
+
+  geom_point(data = df,
+             aes(colour = label),
+             alpha = ifelse(smooth == TRUE, alpha_val, 1),
+             stroke = 0)
+}
+
+#' @title DrWhy's wrapper for geom_smooth function
+#'
+#' @description Function which draws smooth layers in desired order
+#'
+#' @param df Data frame prepared by (\code{make_dataframe}) function
+drwhy_geom_smooth <- function(df) {
+  df$ord <- paste(rev(as.numeric(df$label)), df$label)
+
+  geom_smooth(data = df,
+              aes(group = ord, colour = label),
+              stat = "smooth",
+              method = "gam",
+              formula = y ~ s(x, bs = "cs"),
+              se = FALSE,
+              size = 1,
+              show.legend = TRUE)
+}
