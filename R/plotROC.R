65301fc552a3af5390f54ff4625d687e3c318fd6
Tomasz Mikołajczyk <tmikolajczyk@users.noreply.github.com>
2019-07-14 08:50:46 +0200

    Plot roc et al (#100)
    * plot_roc, documentation and support function
    
    * plot_model_correlation - removing unecessary subfunction
    
    * plot_lift, plot_roc and plot_residual_boxplot - some minor changes

--- R/plotROC.R
+++ R/plotROC.R
@@ -5,6 +5,7 @@
 #'
 #' @param object An object of class ModelAudit or modelEvaluation.
 #' @param ... Other modelAudit objects to be plotted together.
+#' @param nlabels Number of cutoff points to show on the plot. Default is `NULL`.
 #'
 #' @return ggplot object
 #'
@@ -25,18 +26,41 @@
 #' @export
 
 
-plotROC <- function(object, ...){
-  if(!("modelEvaluation" %in% class(object) || "modelAudit" %in% class(object))) stop("The function requires an object created with audit() or modelEvaluation().")
-  if(!("modelEvaluation" %in% class(object))) object <- modelEvaluation(object)
-  label <- fpr <- tpr <- NULL
+plotROC <- function(object, ..., nlabels = NULL) {
 
+  label <- fpr <- tpr <- ord <- cutoffs <- NULL
+
+  # check if passed object is of class "modelResiduals" or "modelAudit"
+  check_object(object, type = "eva")
+
+  # prepare data frame for the ggplot object
   df <- make_dataframe(object, ..., type = "eva")
 
-  ggplot(df, aes(x = fpr, y = tpr, color = label)) +
-    geom_step() +
-    xlab("false positive fraction") +
-    ylab("true positive fraction") +
-    ggtitle("ROC Curve") +
-    theme_light()
-}
+  # if cutoff points should be placed on the chart
+  n_models  <- length(unique(df$label))
+  len_model <- nrow(df) / n_models
+  inds <- c()
+  if (!is.null(nlabels)) {
+    inds <- floor(seq(1, len_model, length.out = nlabels))
+    inds <- c(inds, as.vector(sapply(2:n_models, function(x) c(inds + (len_model * (x - 1))))))
+  }
 
+  # new varibale to set an order o curves
+  df$ord <- paste(rev(as.numeric(df$label)), df$label)
+
+  # colors for model(s)
+  colours <- rev(theme_drwhy_colors(n_models))
+
+  p <- ggplot(data = df, aes(x = fpr, y = tpr, color = label, group = ord)) +
+    geom_step() +
+    geom_point(data = df[inds,], show.legend = FALSE) +
+    geom_text_repel(data = df[inds,], aes(label = format(round(cutoffs, 2), nsmall = 2)), show.legend = FALSE, size = 3.5)
+
+  # theme, colours, titles, axes, scales, etc.
+  p + theme_drwhy() +
+    theme(axis.line.x = element_line(color = "#371ea3")) +
+    scale_color_manual(values = rev(colours), breaks = levels(df$label), guide = guide_legend(nrow = 1)) +
+    xlab("False positive fraction") +
+    ylab("True positive fraction") +
+    ggtitle("ROC curve")
+}
