87490b2aff21a6b790ebd46fa5785ea74391bdcf
jusira <alicjagosiewska@gmail.com>
2019-07-30 17:15:58 +0200

    remove audit() and company

--- R/predict_functions.R
+++
@@ -1,7 +0,0 @@
-yhat <- function(X.model, newdata, ...) {
-  if ("lm" %in% class(X.model)) {
-    stats::predict(X.model, newdata, ...)
-  } else {
-    as.numeric(stats::predict(X.model, newdata, ...))
-  }
-}
