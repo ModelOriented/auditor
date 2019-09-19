73cb94d449efe3ee051b526cb32376d8c6537d21
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2019-09-19 12:33:31 +0200

    New scores for classification

---
+++ R/score_one_minus_F1.R
@@ -0,0 +1,39 @@
+#' @title One minus F1 score
+#'
+#'
+#' @param object An object of class 'explainer' created with function \code{\link[DALEX]{explain}} from the DALEX package.
+#'
+#' @return An object of class 'auditor_score'.
+#'
+#' @examples
+#' titanic <- na.omit(DALEX::titanic)
+#' titanic$survived <- titanic$survived == "yes"
+#'
+#' # fit a model
+#' model_glm <- glm(survived ~ ., family = binomial, data = titanic)
+#'
+#' #create an explainer
+#' exp_glm <- DALEX::explain(model_glm, y = titanic$survived)
+#'
+#' # calculate score
+#' score_one_minus_F1(exp_glm)
+#'
+#'
+#' @export
+
+
+score_one_minus_F1 <- function(object){
+  if(!("explainer" %in% class(object))) stop("The function requires an object created with explain() function from the DALEX package.")
+
+  conf <- confusionmatrix(object)
+
+  ret <- (2 * (conf$TP / (conf$TP + conf$FP)) * (conf$TP / (conf$TP + conf$FN))) /
+    (conf$TP / (conf$TP + conf$FN) + conf$TP / (conf$TP + conf$FP))
+  F1_results <- list(
+    name = "one_minus_F1",
+    score = ret
+  )
+
+  class(F1_results) <- "auditor_score"
+  return(F1_results)
+}
---
+++ R/scores_support_functions.R
@@ -0,0 +1,13 @@
+confusionmatrix <- function(explainer) {
+  yhat <- as.numeric(explainer$y_hat > 0.5)
+  TP <- sum(yhat[yhat == 1] == explainer$y[yhat == 1])
+  FP <- length(yhat[yhat == 1]) - TP
+  TN <- sum(yhat[yhat == 0] == explainer$y[yhat == 0])
+  FN <- length(yhat[yhat == 0]) - TN
+  list(
+    "TP" = TP,
+    "FP" = FP,
+    "TN" = TN,
+    "FN" = FN
+  )
+}
