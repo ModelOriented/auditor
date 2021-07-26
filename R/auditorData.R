c6d8547071305ae4ecf5e09e14e55e8c989ae07e
jusira <alicjagosiewska@gmail.com>
2021-07-26 14:08:55 +0200

    Update documentation for auditorData

--- R/auditorData.R
+++ R/auditorData.R
@@ -5,8 +5,11 @@
 #'
 #' @name auditorData
 #' @docType data
+#' @usage data(auditorData)
+#' @keywords auditorData
 #'
 #' @examples
 #' data("auditorData", package = "auditor")
 #' head(auditorData)
+#' @format a data frame with 2000 rows and 5 columns
 NULL
